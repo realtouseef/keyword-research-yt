f4a7e1bd6aae2e651a6fb4c21a1c01486ea517cf
realtouseef <touseef69r@gmail.com>
2022-10-10 16:04:39 +0500

    basic styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
 import { Box, Heading } from "@chakra-ui/react";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <Box>
-      <Heading>NextJs, Chakra-ui, TypeScript template🤩</Heading>
+    <Box maxW="8xl" mx="auto" textAlign="center" mt="20px">
+      <Head>
+        <title>NextJS, Chakra-ui, TypeScript template🤩</title>
+      </Head>
+
+      <Heading>NextJS, Chakra-ui, TypeScript template🤩</Heading>
     </Box>
   );
 };
