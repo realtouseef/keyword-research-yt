ac9ccf86d35cf43ecf2b0847a5287ade73edef8e
realtouseef <touseef69r@gmail.com>
2022-10-10 15:58:13 +0500

    base

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Box, Heading } from "@chakra-ui/react";
+
+const Home: NextPage = () => {
+  return (
+    <Box>
+      <Heading>NextJs, Chakra-ui, TypeScript template🤩</Heading>
+    </Box>
+  );
+};
+
+export default Home;
