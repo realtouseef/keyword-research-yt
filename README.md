641b5d2f8cc803e4a412a301dd889845ca8db741
realtouseef <touseef69r@gmail.com>
2022-10-24 19:09:30 +0500

    fix: vercel err, and readme

--- README.md
+++ README.md
@@ -8,12 +8,10 @@ A Keyword Research Tool — consuming YT API
 - TypeScript
 - Chakra-ui
 
-## TODOs
+## How to use?
 
-## comments
+- Paste an `url` of a video in the search field and click `Search for Keywords`
 
-the video endpoint `https://www.googleapis.com/youtube/v3/videos` uses `id` to identify a video and returns the data, ie: title, description, tags, channel name, etc.
+## Functionalities
 
-**consumes the `API key`, without it, you can't make any requests**
-
-- Quota impact: A call to this method has a quota cost of 1 unit.
+1. Shows keywords available for a video
--- src/components/Video/FetchVideoKeywords.tsx
+++ src/components/Video/FetchVideoKeywords.tsx
@@ -19,65 +19,67 @@ type YTProps = {
 };
 
 export const FetchVideoKeywords = ({ url }: { url: string }): JSX.Element => {
-  const { isLoading, data, refetch } = useQuery(["ytData"], () =>
+  const { data, refetch } = useQuery(["ytData"], () =>
     fetch(url).then((res) => res.json())
   );
 
-  // if (!data) return <Text>Loading Results....</Text>;
+  function fetchKeywords() {
+    return refetch();
+  }
 
   return (
     <Box>
-      <Button my="20px" onClick={refetch}>
+      <Button my="20px" onClick={fetchKeywords}>
         Search for keywords
       </Button>
-      {data &&
-        data?.items.map(({ id, snippet }: YTProps) => {
-          return (
-            <Box key={id} my="40px">
-              <Image
-                src={snippet.thumbnails.high.url}
-                alt={snippet.title}
-                width={snippet.thumbnails.high.width}
-                height={snippet.thumbnails.high.height}
-              />
-              <Heading fontSize="24px">{snippet.title}</Heading>
-              <Box py="40px">
-                <Flex alignItems="center" justifyContent="start">
-                  <Heading mr="10px" fontSize="30px">
-                    Keywords:
-                  </Heading>
-                  <Highlight
-                    query={
-                      `${snippet.tags}`
-                        ? `${snippet?.tags?.length} keywords found`
-                        : "No keywords found"
-                    }
-                    styles={{
-                      px: "1",
-                      py: "1",
-                      rounded: "md",
-                      bg: "green.200",
-                    }}
-                  >
-                    {`${snippet.tags}`
+
+      {data?.items.map(({ id, snippet }: YTProps) => {
+        return (
+          <Box key={id} my="40px">
+            <Image
+              src={snippet.thumbnails.high.url}
+              alt={snippet.title}
+              width={snippet.thumbnails.high.width}
+              height={snippet.thumbnails.high.height}
+            />
+            <Heading fontSize="24px">{snippet.title}</Heading>
+            <Box py="40px">
+              <Flex alignItems="center" justifyContent="start">
+                <Heading mr="10px" fontSize="30px">
+                  Keywords:
+                </Heading>
+                <Highlight
+                  query={
+                    `${snippet.tags}`
                       ? `${snippet?.tags?.length} keywords found`
-                      : "No keywords found"}
-                  </Highlight>
-                </Flex>
-                <Box>
-                  {snippet.tags ? (
-                    snippet.tags.join(", ")
-                  ) : (
-                    <Text>
-                      The video&apos;s creator has not added any keywords. Check
-                      for another video.
-                    </Text>
-                  )}
-                </Box>
+                      : "No keywords found"
+                  }
+                  styles={{
+                    px: "1",
+                    py: "1",
+                    rounded: "md",
+                    bg: "green.200",
+                  }}
+                >
+                  {`${snippet.tags}`
+                    ? `${snippet?.tags?.length} keywords found`
+                    : "No keywords found"}
+                </Highlight>
+              </Flex>
+              <Box>
+                {snippet.tags ? (
+                  snippet.tags.join(", ")
+                ) : (
+                  <Text>
+                    The video&apos;s creator has not added any keywords. Check
+                    for another video.
+                  </Text>
+                )}
               </Box>
             </Box>
-          );
-        })}
+          </Box>
+        );
+      })}
     </Box>
   );
 };
